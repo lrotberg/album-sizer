ca4214d134638baca9b6cc4713a30eec131e5748
Lior Rotberg <Exolite@gmail.com>
2024-05-25 17:48:44 +0300

    Add Chakra UI Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
-import Fraction from "./components/Fraction";
-import { createFraction } from "./helperFunctions";
+import MainForm from "./components/MainForm";
 
 function App() {
-  const { top, bottom, wholeNumber } = createFraction(5.6144);
-  return <Fraction top={top} bottom={bottom} wholeNumber={wholeNumber} />;
+  // const { top, bottom, wholeNumber } = createFraction(5.6144);
+  // return <Fraction top={top} bottom={bottom} wholeNumber={wholeNumber} />;
+  return (
+    <Grid placeItems="center" templateAreas={"left form right"} color="blackAlpha.700" dir="rtl">
+      <GridItem id="left" w="100%" h="100%" colSpan={2} bg="gray.100" area={"left"} />
+      <GridItem
+        id="form"
+        w="100%"
+        colSpan={8}
+        area={"form"}
+        borderStart={"2px solid"}
+        borderStartColor={"gray.300"}
+        borderEnd={"2px solid lightgray"}
+        borderEndColor={"gray.300"}
+      >
+        <MainForm />
+      </GridItem>
+      <GridItem id="right" w="100%" h="100%" colSpan={2} bg="gray.100" area={"right"} />
+    </Grid>
+  );
 }
 
 export default App;
