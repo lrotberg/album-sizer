6e99790500a18d8e037c1740c226785ff0b6a7b3
Lior Rotberg <Exolite@gmail.com>
2024-05-26 02:08:51 +0300

    Add form fields for page orientation and size

--- src/components/MainForm.tsx
+++ src/components/MainForm.tsx
@@ -3,12 +3,15 @@ import {
   Button,
   FormControl,
   FormErrorMessage,
+  FormHelperText,
   HStack,
+  Input,
   Radio,
   RadioGroup,
   Select,
   VStack
 } from "@chakra-ui/react";
+import { useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import words from "../words.json";
 import FormLabel from "./FormLabel";
@@ -16,6 +19,9 @@ import FormLabel from "./FormLabel";
 interface FormData {
   imageSize: string;
   pageCount: number;
+  height: number;
+  width: number;
+  orientation: string;
 }
 
 const MainForm = () => {
@@ -25,8 +31,23 @@ const MainForm = () => {
     formState: { errors }
   } = useForm<FormData>();
 
+  const [orientation, setOrientation] = useState<"vertical" | "horizontal">("vertical");
+
+  const handleOrientationChange = (value: "vertical" | "horizontal") => {
+    setOrientation(value);
+  };
+
   const onSubmit = (data: FieldValues) => {
     console.log(data);
+  };
+
+  const setOrientationInput = (registerValue: "height" | "width") => {
+    return {
+      w: "80px",
+      dir: "ltr",
+      placeholder: "4 5/8",
+      ...register(registerValue, { min: 4, max: 11, required: true })
+    };
   };
 
   return (
@@ -69,6 +90,55 @@ const MainForm = () => {
             {errors.pageCount?.type === "required" && (
               <FormErrorMessage>{words.required}</FormErrorMessage>
             )}
+          </FormControl>
+          <FormControl isInvalid={errors.orientation?.type === "required"}>
+            <FormLabel htmlFor="pageOrientation" text={words.pageOrientationLabel} />
+            <RadioGroup
+              id="pageOrientation"
+              defaultValue="vertical"
+              onChange={handleOrientationChange}
+            >
+              <HStack>
+                <Radio value="vertical" {...register("orientation", { required: true })}>
+                  {words.vertical}
+                </Radio>
+                <Radio value="horizontal" {...register("orientation", { required: true })}>
+                  {words.horizontal}
+                </Radio>
+              </HStack>
+            </RadioGroup>
+            {errors.orientation?.type === "required" && (
+              <FormErrorMessage>{words.required}</FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl
+            isInvalid={errors.height?.type === "required" || errors.width?.type === "required"}
+          >
+            <VStack align={"start"}>
+              <FormHelperText>{words.pageSizeExplanation}</FormHelperText>
+              <HStack>
+                <FormLabel htmlFor="width" text={words.width} />
+                <Input id="width" {...setOrientationInput("width")} />
+                {errors.width?.type === "required" && (
+                  <FormErrorMessage>{words.required}</FormErrorMessage>
+                )}
+              </HStack>
+              <HStack>
+                {orientation === "vertical" ? (
+                  <Box h="120px" w="80px" bg={"gray.300"} border={"1px"} borderColor={"gray.400"} />
+                ) : (
+                  <Box h="80px" w="120px" bg={"gray.300"} border={"1px"} borderColor={"gray.400"} />
+                )}
+
+                <VStack justifyContent={"center"}>
+                  <FormLabel htmlFor="height" text={words.height} />
+                  <Input id="height" {...setOrientationInput("height")} />
+                  {errors.height?.type === "required" && (
+                    <FormErrorMessage>{words.required}</FormErrorMessage>
+                  )}
+                </VStack>
+              </HStack>
+            </VStack>
           </FormControl>
           <FormControl>
             <Button type="submit">{words.submit}</Button>
