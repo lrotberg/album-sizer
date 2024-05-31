dcf834c742f1e9744454464fc3c8c2e3bb41d3c4
Lior Rotberg <Exolite@gmail.com>
2024-05-31 17:43:10 +0300

    Refactor: Update Opening class to use property assignment instead of constructor parameters

--- src/classes/Opening.ts
+++ src/classes/Opening.ts
@@ -1,8 +1,27 @@
 import { Page, Photo } from "../interfaces";
 
 export default abstract class Opening {
-  constructor(readonly page: Page, readonly photo: Photo) { }
+  page: Page;
+  photo: Photo;
+
+  constructor(page: Page, photo: Photo) {
+    this.page = page;
+    this.photo = photo;
+  }
+
   abstract getName: () => string;
+
   abstract getHeight: () => number;
+
   abstract getWidth: () => number;
+
+  setPhotoSize = (width: number, height: number) => {
+    this.photo.size.height = height;
+    this.photo.size.width = width;
+  };
+
+  setPageSize = (width: number, height: number) => {
+    this.page.size.height = height;
+    this.page.size.width = width;
+  };
 }
